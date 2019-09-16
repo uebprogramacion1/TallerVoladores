83931b21955fcde8fc5273cbbe828aa7ef34d286
Aula Informática B213 <aulab213@salas.edu.co>
2019-09-16 09:51:06 -0500

    Cambio en el main

--- src/controlador/main.java
+++ src/controlador/main.java
@@ -4,6 +4,7 @@ public class main {
 
 	public static void main(String[] args) {
 		System.out.println("Comienza programa");
+		System.out.println("Una línea más");
 		System.out.println("Termina programa");
 	}
 
